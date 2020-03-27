10053c8a2429bdf05be52d86d62854cdc6220ed2
BLTinAnnArbor <tbl101@wccnet.edu>
2020-03-26 20:23:21 -0400

    update

--- assignments/assignment6/assignment6.php
+++ assignments/assignment6/assignment6.php
@@ -1,19 +1,17 @@
 <?php
 
-require_once 'fileUploadProc.php';
+require_once 'FileUploadProc.php';
 $output = "";
 
-if(isset($_POST['submitButton']))  { $submitted = true;  }
-else { $submitted = false; }
+if (isset($_POST['submitButton'])) {
+  $submitted = true;
+} else {
+  $submitted = false;
+}
 
-if($submitted){
+if ($submitted) {
   $upload = new Upload();
   $output = $upload->checkFile();
-
-  if($upload->checkIfError()== false){
-    echo "It works here";
-    $output = $upload->moveFile();
-  }
 }
 
 ?>
@@ -27,41 +25,44 @@ if($submitted){
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
   <title>File Upload Assignment</title>
 </head>
 
 <body>
-    
-  <div class="container">
-    
-    <h2>File Upload</h2>
-    
-      <?php 
-    
-      echo $output;
-      
-      ?>
 
-      <br><br>
+  <div class="container">
+
+    <h2>File Upload</h2>
+
+    <?php
+
+    $file = "displayLinks.php";
+
+    echo "<a href= $file>Show file list</a>";
+
+    echo $output;
+
+    ?>
+
+    <br><br>
 
     <form action="assignment6.php" method="post" enctype="multipart/form-data">
-    
-        <div class="form-group">
-          <label for="fileName">File Name</label>
-          <input type="text" class="form-control" name="enteredFileName" id="enteredFileName">
-        </div>
 
-        <div class="form-group">
-          <label for="selectedFile" ></label>
-          <input type="file" name="selectedFile" id="selectedFile">
-        </div>
-        
+      <div class="form-group">
+        <label for="fileName">File Name</label>
+        <input type="text" class="form-control" name="enteredFileName" id="enteredFileName">
+      </div>
 
-        <input class="btn btn-primary" type="submit" name="submitButton" id="submitButton" value="Upload File" >
-      </form>
+      <div class="form-group">
+        <label for="selectedFile"></label>
+        <input type="file" name="selectedFile" id="selectedFile">
+      </div>
+
+
+      <input class="btn btn-primary" type="submit" name="submitButton" id="submitButton" value="Upload File">
+    </form>
 
   </div>
 </body>
--- assignments/assignment6/fileUploadProc.php
+++ assignments/assignment6/fileUploadProc.php
@@ -5,46 +5,38 @@ class Upload{
     private $fileName;
     private $fileSize;
     private $fileType;
-    private $error;
-
+   
     function __construct() {
 
         $this->fileSize = $_FILES["selectedFile"]["size"]; 
         $this->fileType = $_FILES["selectedFile"]["type"];
         $this->fileName = $_FILES["selectedFile"]["name"];
-        $this->error = false;
     }
 
     function checkFile(){  
-        if($this->fileSize > 1000000){
-            return "File too big.";
+        
+        if($this->fileSize > 100000){
+            return "This file is too big.";
         }
         else if($this->fileType != "application/pdf"){
-            return "File not okay, it must be a pdf file.";
+            return "File type is not okay. It must be a pdf file.";
         }else{
             return $this->moveFile();
         }
         
-        //return "File Size is: {$this->fileSize}. File Type is {$this->fileType}";
+       // return "File Size is: {$this->fileSize}. File Type is {$this->fileType}";
     }
 
-    function checkIfError(){
-        if($this->error = true){
-            return true;
-        }else{
-            return false;
-        }
-    }
 
     function moveFile(){
 
         $fileName = $_POST['enteredFileName'];
 
-        if(move_uploaded_file($_FILES["selectedFile"]["tmp_name"], 
-        "files/".$fileName)){
+        if(move_uploaded_file($_FILES["selectedFile"]["tmp_name"], "files/".$fileName)){
+
+            // Insert things into table here
             return "Your file was successfully uploaded.";
         }else{
-            $this->error = true;  // necessary here??
             return "There was a problem uploading your file. Please try again.";
         }
 
